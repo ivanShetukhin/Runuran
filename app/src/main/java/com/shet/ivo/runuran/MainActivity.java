03dcf13db51f2e7ac6c8d3928ae01a1ad2526c4c
ivoshet <ivan.v.shet@gmail.com>
2017-08-04 16:52:56 +0700

    added clear button and scroll view

--- app/src/main/java/com/shet/ivo/runuran/MainActivity.java
+++ app/src/main/java/com/shet/ivo/runuran/MainActivity.java
@@ -3,11 +3,16 @@ package com.shet.ivo.runuran;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences list_of_ran_distance;
     private EditText input_distance;
+    private Button save_btn;
+    private Button clear_btn;
+    private ScrollView distance_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
     private void setElements()
     {
         input_distance = (EditText) findViewById(R.id.input_distance);
+        save_btn = (Button) findViewById(R.id.save_btn);
+        clear_btn = (Button) findViewById(R.id.clear_btn);
+        distance_list = (ScrollView) findViewById(R.id.distance_list);
     }
 
 

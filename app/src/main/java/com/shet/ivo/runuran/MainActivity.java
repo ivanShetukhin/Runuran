6c761c9b20feb81556c67bf683428158fedbcd18
ivoshet <ivan.v.shet@gmail.com>
2017-08-04 16:36:28 +0700

    added edit text, button

--- app/src/main/java/com/shet/ivo/runuran/MainActivity.java
+++ app/src/main/java/com/shet/ivo/runuran/MainActivity.java
@@ -1,13 +1,31 @@
 package com.shet.ivo.runuran;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences list_of_ran_distance;
+    private EditText input_distance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //search have ran distance
+        list_of_ran_distance = getSharedPreferences("distance", MODE_PRIVATE);
     }
+    //set elements
+    private void setElements()
+    {
+        input_distance = (EditText) findViewById(R.id.input_distance);
+    }
+
+
+
+
+
+
 }
